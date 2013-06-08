764905838c92beeca5671c48c5f8b4bb4a37b853
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-06-08 18:22:35 +0200

    Augment rt_task() with is_realtime()
    Whenever the kernel checks for rt_task() to avoid delaying real-time
    tasks, we want it to also not delay LITMUS^RT tasks.  Hence, most
    calls to rt_task() should be matched by an equivalent call to
    is_realtime().
    
    Notably, this affects the implementations of select() and nanosleep(),
    which use timer_slack_ns when setting up timers for non-real-time
    tasks.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -37,6 +37,8 @@
 #include <linux/timer.h>
 #include <trace/events/writeback.h>
 
+#include <litmus/litmus.h> /* for is_realtime() */
+
 /*
  * Sleep at most 200ms at a time in balance_dirty_pages().
  */
@@ -275,7 +277,7 @@ void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty)
 	if (background >= dirty)
 		background = dirty / 2;
 	tsk = current;
-	if (tsk->flags & PF_LESS_THROTTLE || rt_task(tsk)) {
+	if (tsk->flags & PF_LESS_THROTTLE || rt_task(tsk) || is_realtime(tsk)) {
 		background += background / 4;
 		dirty += dirty / 4;
 	}
@@ -329,7 +331,7 @@ static unsigned long zone_dirty_limit(struct zone *zone)
 	else
 		dirty = vm_dirty_ratio * zone_memory / 100;
 
-	if (tsk->flags & PF_LESS_THROTTLE || rt_task(tsk))
+	if (tsk->flags & PF_LESS_THROTTLE || rt_task(tsk) || is_realtime(tsk))
 		dirty += dirty / 4;
 
 	return dirty;
--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -59,6 +59,8 @@
 #include <linux/migrate.h>
 #include <linux/page-debug-flags.h>
 
+#include <litmus/litmus.h> /* for is_realtime() */
+
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
 #include "internal.h"
@@ -2326,7 +2328,8 @@ gfp_to_alloc_flags(gfp_t gfp_mask)
 		 * See also cpuset_zone_allowed() comment in kernel/cpuset.c.
 		 */
 		alloc_flags &= ~ALLOC_CPUSET;
-	} else if (unlikely(rt_task(current)) && !in_interrupt())
+	} else if (unlikely(rt_task(current) || is_realtime(current))
+		   && !in_interrupt())
 		alloc_flags |= ALLOC_HARDER;
 
 	if (likely(!(gfp_mask & __GFP_NOMEMALLOC))) {
