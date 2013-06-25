a4f0a287af5998d847f233e85173b182aea1e11e
Bjoern Brandenburg <bbb@mpi-sws.org>
2013-06-25 07:32:20 +0200

    Add object descriptor table to Linux's task_struct
    This table is similar to a file descriptor table. It keeps track of
    which "objects" (locks) a real-time task holds a handle to.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1212,6 +1212,7 @@ struct sched_rt_entity {
 };
 
 struct rcu_node;
+struct od_table_entry;
 
 enum perf_event_task_context {
 	perf_invalid_context = -1,
@@ -1532,6 +1533,9 @@ struct task_struct {
 
 	/* LITMUS RT parameters and state */
 	struct rt_param rt_param;
+
+	/* references to PI semaphores, etc. */
+	struct od_table_entry *od_table;
 
 #ifdef CONFIG_LATENCYTOP
 	int latency_record_count;
