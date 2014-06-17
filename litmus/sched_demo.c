75761cd57ea75bf81c69ff12de9661df5b411fad
Aaron Block <ablock@austincollege.edu>
2014-06-17 18:48:44 -0500

    added logging to demo

--- litmus/sched_demo.c
+++ litmus/sched_demo.c
@@ -1,5 +1,24 @@
 #include <litmus/sched_plugin.h>
 #include <litmus/preempt.h>
+#include <litmus/debug_trace.h>
+
+#include <linux/percpu.h>
+
+#include <litmus/litmus.h>
+#include <litmus/rt_domain.h>
+#include <litmus/edf_common.h>
+
+struct demo_cpu_state {
+        rt_domain_t     local_queues;
+        int             cpu;
+
+        struct task_struct* scheduled;
+};
+
+static DEFINE_PER_CPU(struct demo_cpu_state, demo_cpu_state);
+
+#define cpu_state_for(cpu_id)   (&per_cpu(demo_cpu_state, cpu_id))
+#define local_cpu_state()       (&__get_cpu_var(demo_cpu_state))
 
 static struct task_struct* demo_schedule(struct task_struct * prev)
 {
@@ -13,8 +32,28 @@ static struct task_struct* demo_schedule(struct task_struct * prev)
         return NULL;
 }
 
+static long demo_activate_plugin(void)
+{
+        int cpu;
+        struct demo_cpu_state *state;
+
+        for_each_online_cpu(cpu) {
+                TRACE("Initializing CPU%d...\n", cpu);
+
+                state = cpu_state_for(cpu);
+
+                state->cpu = cpu;
+                state->scheduled = NULL;
+                edf_domain_init(&state->local_queues, NULL, NULL);
+        }
+
+        return 0;
+}
+
 static long demo_admit_task(struct task_struct *tsk)
 {
+		TRACE_TASK(tsk, "rejected by demo plugin.\n");
+		
         /* Reject every task. */
         return -EINVAL;
 }
@@ -23,6 +62,7 @@ static struct sched_plugin demo_plugin = {
         .plugin_name            = "DEMOY2",
         .schedule               = demo_schedule,
         .admit_task             = demo_admit_task,
+        .activate_plugin        = demo_activate_plugin,
 };
 
 static int __init init_demo(void)
